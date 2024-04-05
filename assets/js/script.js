9664dc441cee1eb6bf2d510bea8cdb247105ca22
Clarissa Mobley <clarissamobley@me.com>
2024-04-05 17:39:56 -0400

    Added fetch to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,31 +5,52 @@ const apiKey = "530886ee7df4842ed6caba305a22369e";
 
 let currentDate = new Date().toLocaleDateString();
 
-const searchButtonHandler = function(event) {
-    event.preventDefault();
+const searchButtonHandler = function (event) {
+  event.preventDefault();
 
-    const city = userSearch.value.trim();
+  const city = userSearch.value.trim();
 
-    if (city) {
-        getWeather(city);
-    } else {
-        alert("Please enter a city name");
-    }
+  if (city) {
+    getWeather(city);
+  } else {
+    alert("Please enter a city name");
+  }
 };
 
-const displayWeather = function(data, city) {
-    currentForecast.innerHTML = `
-        <h1 class="fw-bold">${data.name} (${currentDate})</h1>
-        </br>temp:${data.main.temp}`;
+const getWeather = function (city) {
+  const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+  fetch(currentWeatherUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayWeather(data, city);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function () {
+      alert("Unable to connect to OpenWeather");
+    });
 };
 
+const displayWeather = function (data, city) {
+  const temperatureFahrenheit = ((data.main.temp - 273.15) * 9) / 5 + 32;
+
+  currentForecast.innerHTML = `
+          <h1 class="fw-bold">${data.name} (${currentDate})</h1>
+          </br>temp: ${temperatureFahrenheit.toFixed(2)}°F
+          </br>wind: ${data.wind.speed} MPH
+          </br>humidity: ${data.main.humidity}%`;
+};
 
 
 //   for (let i = 5; i < forecastCards.length; i++) {
 //     document.querySelector(".forecastCards").innerHTML += `
 //     <div class="card" style="width: 18rem;">
 //     <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
+//     <h5 class="card-title"></h5>
 //     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
 //     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 //     <a href="#" class="card-link">Card link</a>
@@ -37,4 +58,3 @@ const displayWeather = function(data, city) {
 //     </div>
 //     </div>`;
 //   }
-
