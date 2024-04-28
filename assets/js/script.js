fe9c1a174716fdc7f780877051acb7e15fa9683e
Clarissa Mobley <clarissamobley@me.com>
2024-04-28 13:38:41 -0400

    Changed five day forecast date so it starts the day after

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ function fetchCityData(city) {
     .then((data) => {
       displayWeather(data);
       saveSearch(city);
-      getForecast(city)
+      getForecast(city);
     })
     .catch((error) => {
       console.log("Error fetching data: " + error);
@@ -60,15 +60,14 @@ const displayWeather = (data) => {
   </br>humidity: ${data.main.humidity}%</p>`;
 };
 
-
 // Display five day forecast on cards
 const displayForecast = function (data) {
-  console.log(data);
   forecastCards.innerHTML = "";
 
   for (let i = 0; i < 5; i++) {
     const forecast = data.list[i * 8];
-    const date = new Date(forecast.dt * 1000);
+    const date = new Date();
+    date.setDate(date.getDate() + (i + 1));
     const temperatureFahrenheit = ((forecast.main.temp - 273.15) * 9) / 5 + 32;
     const iconCode = forecast.weather[0].icon;
     const iconURL = `http://openweathermap.org/img/wn/${iconCode}.png`;
@@ -146,174 +145,3 @@ function loadCities() {
     addCityButton(city);
   });
 }
-
-// const searchButtonHandler = (e) => {
-//   event.preventDefault();
-//   const city = userSearch.value.trim();
-//   if (city) {
-//     for (let i = 0; i < 8 )
-//   }
-// }
-
-// const userSearch = document.querySelector("#city-search");
-// const searchButton = document.querySelector(".search-btn");
-// const currentForecast = document.querySelector(".current-forecast");
-
-// //API key
-// const apiKey = "530886ee7df4842ed6caba305a22369e";
-
-// //Get current date
-// let currentDate = new Date().toLocaleDateString();
-
-// //Saves last searched city in local storage
-// const lastSearched = localStorage.getItem("lastSearched");
-// const updateLastSearched = (city) => {
-//   localStorage.setItem("lastSearched", city);
-// };
-
-// //Load default Atlanta forecast
-// const loadDefaultForecast = () => {
-//   getWeather("Atlanta");
-// };
-
-//   //Search button handler event
-//   const searchButtonHandler = function (event) {
-//   event.preventDefault();
-//   const city = userSearch.value.trim();
-//   if (city) {
-//     getWeather(city);
-//   } else {
-//     alert("Please enter a city name");
-//   }
-// };
-
-//   //Fetch weather data
-//   const getWeather = function (city) {
-//   const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-//   fetch(currentWeatherUrl)
-//     .then(function (response) {
-//       if (response.ok) {
-//         response.json().then(function (data) {
-//           displayWeather(data, city);
-//           updateLastSearched(city);
-//         });
-//       } else {
-//         alert("Error: " + response.statusText);
-//       }
-//     })
-//     .catch(function () {
-//       alert("Unable to connect to OpenWeather");
-//     });
-// };
-
-//   //Display current weather info in the top box
-//   const displayWeather = function (data, city) {
-//   const temperatureFahrenheit = ((data.main.temp - 273.15) * 9) / 5 + 32;
-//   const iconCode = data.weather[0].icon;
-//   const iconURL = `http://openweathermap.org/img/wn/${iconCode}.png`;
-
-//   const icon = document.createElement("img");
-//   icon.src = iconURL;
-//   icon.alt = "Weather Icon";
-
-//   currentForecast.innerHTML = `
-//           <h1 class="fw-bold m-2">${data.name} (${currentDate})</h1>
-//           <div>${icon.outerHTML}</div>
-//           <p class="m-2">temp: ${temperatureFahrenheit.toFixed(2)}°F
-//           </br>wind: ${data.wind.speed} MPH
-//           </br>humidity: ${data.main.humidity}%</p>`;
-
-//   getForecast(city);
-// };
-
-// //Displaying 5 day forecast on cards
-// const displayForecast = function (data) {
-//   const forecastCards = document.querySelector(".forecastCards");
-//   forecastCards.innerHTML = "";
-
-//   for (let i = 0; i < 5; i++) {
-//     const forecast = data.list[i * 8];
-//     const date = new Date(forecast.dt * 1000);
-//     const temperatureFahrenheit = ((forecast.main.temp - 273.15) * 9) / 5 + 32;
-//     const iconCode = forecast.weather[0].icon;
-//     const iconURL = `http://openweathermap.org/img/wn/${iconCode}.png`;
-//     const card = document.createElement("div");
-//     card.classList.add("card", "m-3");
-//     card.style.width = "10rem";
-
-//     const cardBody = document.createElement("div");
-//     cardBody.classList.add("card-body");
-
-//     const cardTitle = document.createElement("h5");
-//     cardTitle.classList.add("card-title");
-//     cardTitle.textContent = date.toLocaleDateString("en-US", {
-//       month: "numeric",
-//       day: "numeric",
-//       year: "numeric",
-//     });
-
-//     const icon = document.createElement("img");
-//     icon.src = iconURL;
-//     icon.alt = "Weather Icon";
-
-//     const cardText1 = document.createElement("p");
-//     cardText1.classList.add("card-text");
-//     cardText1.textContent = `Temp: ${temperatureFahrenheit.toFixed(2)}°F`;
-
-//     const cardText2 = document.createElement("p");
-//     cardText2.classList.add("card-text");
-//     cardText2.textContent = `Wind: ${forecast.wind.speed} MPH`;
-
-//     const cardText3 = document.createElement("p");
-//     cardText3.classList.add("card-text");
-//     cardText3.textContent = `Humidity: ${forecast.main.humidity}%`;
-
-//     cardBody.appendChild(cardTitle);
-//     cardBody.appendChild(icon);
-//     cardBody.appendChild(cardText1);
-//     cardBody.appendChild(cardText2);
-//     cardBody.appendChild(cardText3);
-//     card.appendChild(cardBody);
-
-//     forecastCards.appendChild(card);
-//   }
-// };
-
-// //Fetch forecast data
-// const getForecast = function (city) {
-//   const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-
-//   fetch(forecastWeatherUrl)
-//     .then(function (response) {
-//       if (response.ok) {
-//         response.json().then(function (data) {
-//           displayForecast(data);
-//         });
-//       } else {
-//         alert("Error: " + response.statusText);
-//       }
-//     })
-//     .catch(function () {
-//       alert("Unable to connect to OpenWeather");
-//     });
-// };
-
-// //Page refreshes back to last searched OR loads default(Atlanta)
-// if (lastSearched) {
-//   getWeather(lastSearched);
-// } else {
-//   loadDefaultForecast();
-// }
-
-// //Search button click event
-// searchButton.addEventListener("click", searchButtonHandler);
-
-// //City buttons click event
-// const cityButtons = document.querySelectorAll(".city-btn");
-// cityButtons.forEach(function (button) {
-//     button.addEventListener("click", function () {
-//         const city = this.textContent.trim();
-//         getWeather(city);
-//   });
-// });
